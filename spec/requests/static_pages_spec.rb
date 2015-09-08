bfc3313c992ef2b0a2bea9359b162e9113afc782
claudia.vanea <claudia.vanea@visma.com>
2015-09-08 16:41:00 +0300

    Before routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,67 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-
+  
   describe "Home page" do
-   	it "should have the h1 'Visma app'" do
-   		visit '/static_pages/home'
-   		page.should have_selector('h1', :text => 'Visma app')
-    end
-    
+    it "should have the h1 'Visma App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Visma App')
+      end
+
     it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Visma App | Home")
-    end
-    it "it should not have a custom page title" do
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Visma App')
+      end
+
+      it "should not have a custom page title" do
       visit '/static_pages/home'
       page.should have_selector('title', :text => '| Home')
+      end
     end
-    
-  end
 
-  describe "Help page" do
-  	it "should have the h1 'Help'" do
-  	  visit '/static_pages/help'
-  	  page.should have_selector('h1', :text => 'Help')
-    end
-    
-    it "should have the title 'Help'" do
+    describe "Help page" do
+      it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Visma App | Help")
-    end
-  end
+      page.should have_selector('h1', :text => 'Help')
+      end
 
-  describe "About page" do
-    it "should have the h1 'About us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About us')
+    it "should have the base title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Visma App')
+      end
+
+      it "should not have a custom page title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => '| Help')
+      end
     end
-    
-    it "should have the title 'About us'" do
+
+    describe "About page" do
+      it "should have the h1 'About us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => "Ruby on Rails Tutorial Visma App | About us")
+      page.should have_selector('h1', :text => 'About Us')
+      end
+
+    it "should have the base title 'About us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Visma App')
+      end
+
+      it "should not have a custom page title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => '| About')
+      end
     end
-  end
+
+    describe "Contact page" do
+      it "should have the h1 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('h1', :text => 'Contact')
+      end
+      
+      it "should have the title 'Contact'" do
+        visit '/static_pages/contact'
+        page.should have_selector('title', :text => "Ruby on Rails Tutorial Visma App | Contact")
+      end
+    end
 end
-
